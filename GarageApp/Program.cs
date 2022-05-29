1ee5d5474b3d62f63a2837abf2189af536f2a2fc
Renata Pedroso <renatanicollyy1@gmail.com>
2022-05-29 11:36:09 -0300

    Solution without interface

--- GarageApp/Program.cs
+++ GarageApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Globalization;
+using GarageApp.Entities;
 
 namespace GarageApp
 {
@@ -6,7 +7,26 @@ namespace GarageApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE: ");
+            Console.WriteLine(carRental.Invoice);
         }
     }
 }
