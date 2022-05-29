1f7fa443c812ee805cbdefa39976d71e5ef8a0d2
Renata Pedroso <renatanicollyy1@gmail.com>
2022-05-29 11:36:09 -0300

    Solution without interface

--- GarageApp/Entities/CarRental.cs
+++ GarageApp/Entities/CarRental.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GarageApp.Entities
 {
-    internal class CarRental
+    public class CarRental
     {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public Invoice Invoice { get; set; }
+
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle)
+        {
+            Start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+        }
     }
 }
--- GarageApp/Entities/Vehicle.cs
+++ GarageApp/Entities/Vehicle.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GarageApp.Entities
 {
-    internal class Vehicle
+    public class Vehicle
     {
         public string Model { get; set; }
 
